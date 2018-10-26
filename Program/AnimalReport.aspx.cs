e0eb8405f76bf989580524a9315cb4692ffe3bfe
hardgrws <44182513+hardgrws@users.noreply.github.com>
2018-10-25 23:14:08 -0400

    Reuploaded Reports
    Tableau is in there as a placeholder for now.  I'll make them look prettier later

---
+++ Program/AnimalReport.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AnimalReport : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    //protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+    //{
+    //    if (e.Row.RowType == DataControlRowType.DataRow)
+    //    {
+    //        e.Row.Attributes["onclick"] = Page.ClientScript.GetPostBackClientHyperlink(GridView1, "Select$" + e.Row.RowIndex);
+    //        e.Row.ToolTip = "Click to select this row.";
+    //    }
+    //}
+
+    //protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    //{
+    //    foreach (GridViewRow row in GridView1.Rows)
+    //    {
+    //        if (row.RowIndex == GridView1.SelectedIndex)
+    //        {
+    //            row.BackColor = ColorTranslator.FromHtml("#A1DCF2");
+    //            row.ToolTip = string.Empty;
+    //        }
+    //        else
+    //        {
+    //            row.BackColor = ColorTranslator.FromHtml("#FFFFFF");
+    //            row.ToolTip = "Click to select this row.";
+    //        }
+    //    }
+    //}
+}
