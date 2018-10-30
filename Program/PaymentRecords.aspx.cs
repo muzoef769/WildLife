b0c40e9c149c7b5e98c438344ebc9f47940bfc34
hardgrws <44182513+hardgrws@users.noreply.github.com>
2018-10-29 23:20:27 -0400

    Complete Overhaul of Report Pages
    Added Tableau back into the Simple and Animal Reports.  The Payment Record page has a cleaner look as well and reflects Raina's excel spreadsheets more

--- Program/PaymentRecords.aspx.cs
+++ Program/PaymentRecords.aspx.cs
@@ -39,4 +39,77 @@ public partial class PaymentRecords : System.Web.UI.Page
     //        }
     //    }
     //}
+
+    protected void btnOct_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "10";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "10";
+    }
+
+    protected void btnJan_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "1";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "1";
+    }
+
+    protected void btnFeb_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "2";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "2";
+    }
+
+    protected void btnMar_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "3";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "3";
+    }
+
+    protected void btnApr_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "4";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "4";
+    }
+
+    protected void btnMay_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "5";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "5";
+    }
+
+    protected void btnJun_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "6";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "6";
+    }
+
+    protected void btnJul_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "7";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "7";
+    }
+
+    protected void btnAug_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "8";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "8";
+    }
+
+    protected void btnSep_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "9";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "9";
+    }
+
+    protected void btnNov_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "11";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "11";
+    }
+
+    protected void btnDec_Click(object sender, EventArgs e)
+    {
+        SqlDataSource2.SelectParameters["month"].DefaultValue = "12";
+        SqlDataSource3.SelectParameters["month"].DefaultValue = "12";
+    }
+
 }
