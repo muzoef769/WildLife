cb0c95cf53c4266ea3d245128eeb2b057dc5f87a
Edwin Muzo <muzoef@dukes.jmu.edu>
2018-11-04 17:10:08 -0500

    Made changes to AnimalReport and added css for AnimalReportCard and AnimalReportContainer

--- Program/AnimalReport.aspx.cs
+++ Program/AnimalReport.aspx.cs
@@ -7,37 +7,88 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.IO;
 
 public partial class AnimalReport : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!this.IsPostBack)
+        {
+            this.BindGrid();
+        }
+    }
+    protected void SearchByDate(object sender, EventArgs e)
+    {
 
     }
+    protected void BindGrid()
+    {
+        //connection();
+        //query = "select *from Employee";//not recommended this i have written for example,write stored procedure for security  
+        //com = new SqlCommand(query, con);
+        //SqlDataReader dr = com.ExecuteReader();
+        //GridView1.DataSource = dr;
+        //GridView1.DataBind();
+        //con.Close();
 
-    //protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
-    //{
-    //    if (e.Row.RowType == DataControlRowType.DataRow)
-    //    {
-    //        e.Row.Attributes["onclick"] = Page.ClientScript.GetPostBackClientHyperlink(GridView1, "Select$" + e.Row.RowIndex);
-    //        e.Row.ToolTip = "Click to select this row.";
-    //    }
-    //}
+        using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["connString"].ConnectionString))
+        {
+            string sql = "SELECT DISTINCT Animal.AnimalName FROM Animal INNER JOIN AssignAnimal ON Animal.AnimalID = AssignAnimal.AnimalID WHERE AssignAnimal.NewProgramID = 1";
 
-    //protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
-    //{
-    //    foreach (GridViewRow row in GridView1.Rows)
-    //    {
-    //        if (row.RowIndex == GridView1.SelectedIndex)
-    //        {
-    //            row.BackColor = ColorTranslator.FromHtml("#A1DCF2");
-    //            row.ToolTip = string.Empty;
-    //        }
-    //        else
-    //        {
-    //            row.BackColor = ColorTranslator.FromHtml("#FFFFFF");
-    //            row.ToolTip = "Click to select this row.";
-    //        }
-    //    }
-    //}
+            connection.Open();
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    command.Connection = connection;
+                    sda.SelectCommand = command;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        grdViewReport.DataSource = dt;
+                        grdViewReport.DataBind();
+                    }
+                }
+            }
+        }
+    }
+    protected void ExportToExcel()
+    {
+        Response.Clear();
+        Response.Buffer = true;
+        Response.ClearContent();
+        Response.ClearHeaders();
+        Response.Charset = "";
+        string FileName = "Vithal" + DateTime.Now + ".xls";
+        StringWriter strwritter = new StringWriter();
+        HtmlTextWriter htmltextwrtter = new HtmlTextWriter(strwritter);
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        Response.ContentType = "application/vnd.ms-excel";
+        Response.AddHeader("Content-Disposition", "attachment;filename=" + FileName);
+        grdViewReport.GridLines = GridLines.Both;
+        grdViewReport.HeaderStyle.Font.Bold = true;
+        grdViewReport.RenderControl(htmltextwrtter);
+        Response.Write(strwritter.ToString());
+        Response.End();
+    }
+
+    public override void VerifyRenderingInServerForm(Control control)
+    {
+        /* Verifies that the control is rendered */
+    }
+    protected void generateMonthReport(object sender, EventArgs e)
+    {
+        ExportToExcel();
+    }
+    protected void OnPaging(object sender, GridViewPageEventArgs e)
+    {
+        grdViewReport.PageIndex = e.NewPageIndex;
+        this.BindGrid();
+    }
+    protected void generateAnnualReport(object sender, EventArgs e)
+    {
+
+    }
 }
