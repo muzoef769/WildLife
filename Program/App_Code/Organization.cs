cd0229a6ae4adf584f66903ad93458cc4885eadb
Edwin Muzo <muzoef@dukes.jmu.edu>
2018-11-16 13:29:52 -0500

    Organization Changes

--- Program/App_Code/Organization.cs
+++ Program/App_Code/Organization.cs
@@ -10,25 +10,22 @@ public class Organization
 {
     //Attributes
     private int orgID;
-    private int addressID;
-    private int contactID;
     private string orgName;
-
+    private int addressID;
     private DateTime lastUpdated;
     private string lastUpdatedBy;
 
-    public Organization(int addressID, int contactID, string orgName, DateTime lastUpdated, string lastUpdatedBy)
+    public Organization(string orgName, int addressID, DateTime lastUpdated, string lastUpdatedBy)
     {
-        this.addressID = addressID;
-        this.contactID = contactID;
-        this.orgName = orgName;
-        this.lastUpdated = lastUpdated;
-        this.lastUpdatedBy = lastUpdatedBy;
+        this.OrgName = orgName;
+        this.AddressID = addressID;
+        this.LastUpdated = lastUpdated;
+        this.LastUpdatedBy = lastUpdatedBy;
     }
 
-    public int AddressID { get => addressID; set => addressID = value; }
-    public int ContactID { get => contactID; set => contactID = value; }
+    public int OrgID { get => orgID; set => orgID = value; }
     public string OrgName { get => orgName; set => orgName = value; }
+    public int AddressID { get => addressID; set => addressID = value; }
     public DateTime LastUpdated { get => lastUpdated; set => lastUpdated = value; }
     public string LastUpdatedBy { get => lastUpdatedBy; set => lastUpdatedBy = value; }
 }
